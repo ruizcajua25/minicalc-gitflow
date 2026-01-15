048692a94b4475f2c0d8365c0ed61ce64f3d3a99
Juan Ruiz <ruiz.cajua25@triana.salesianos.edu>
2026-01-15 10:39:44 +0100

    fix: hotfix add multiply

--- MiniCalc.java
+++ MiniCalc.java
@@ -2,6 +2,7 @@
 public class MiniCalc {
     public static int add(int a, int b) { return a + b; }
     public static int subtract(int a, int b) { return a - b; }
+    public static int multiply(int a, int b) { return a * b; }
 }
 
 
